754c7cff804f25f80f68ff1c9d291d388afb4564
Sascha-Sigel <68034844+S4kuny@users.noreply.github.com>
2022-06-10 09:03:16 +0200

    mysql setup v1.1

--- setup.sql
+++ setup.sql
@@ -27,12 +27,13 @@ alter table TAusleihen auto_increment = 1000;
 drop table if exists TKunden;
 create table TKunden (
 	KunNummer int unsigned primary key auto_increment,
-    KunAnrede enum('Herr', 'Frau', 'Dr', 'Divers', 'Severin') not null,
+    KunAnrede enum('Herr', 'Frau', 'Divers'),
     KunVorname varchar(40) not null,
     KunNachname varchar(40) not null,
     KunStrasse varchar(50) not null,
+    KunEMail varchar(40) not null,
     KunGebDatum date not null,
-    KunTelefon char(16),
+    KunTelefon char(16) not null,
     OrtONRP int(5) not null
 );
 alter table TKunden auto_increment=1000;
@@ -44,8 +45,5 @@ create table TOrte (
   OrtName varchar(30) not null
 );
 
-show tables;
-describe TAusleihen;
-describe TKunden;
-describe TOrte;
-describe TVideos;
+insert into TKunden values(null, 'Herr', 'Sascha', 'Sigel', 'Weinfelderstrasse 124b', 'sascha.sigel@stud.kftg.ch', '2004-07-06', '+41 71 411 79 15', 4893);
+select * from TKunden;
