0407669021cd7d2a9be52e6900c0311064d49d13
Sascha-Sigel <68034844+S4kuny@users.noreply.github.com>
2022-06-23 11:02:57 +0200

    added creds (credentials for i-kf.ch) and setup.sql updates so it fits the server

--- creds.sql
+++ creds.sql
@@ -1,3 +1,4 @@
 create database if not exists joss_kundenportal default character set utf8mb4 collate utf8mb4_unicode_ci;
 use joss_kundenportal;
-create user 'jossAdmin'@'i-kf.ch' identified by 'zi_23Dt';
+create user 'jossAdmin'@'i-kf.ch' identified by 'zi_23Dt9!y';
+grant all privileges on joss_kundenportal.* to 'jossAdmin'@'i-kf.ch';
