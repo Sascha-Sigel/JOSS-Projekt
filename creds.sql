5c00b878df4093309d00b33ad6e4a1d8a5d4ef52
Sascha-Sigel <68034844+S4kuny@users.noreply.github.com>
2022-06-23 07:09:26 +0200

    added credentials file to store the user and pw for i-kf.ch

---
+++ creds.sql
@@ -0,0 +1,3 @@
+create database if not exists joss_kundenportal default character set utf8mb4 collate utf8mb4_unicode_ci;
+use joss_kundenportal;
+create user 'jossAdmin'@'i-kf.ch' identified by 'zi_23Dt';
