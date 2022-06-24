e08d1a4df8acd028a6c8bf254c4c2133c39e2bac
Sascha-Sigel <68034844+S4kuny@users.noreply.github.com>
2022-06-24 19:29:28 +0200

    maybe db server creds will change

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'i-kf.ch');
-define('DB_USERNAME', 'jossAdmin');
-define('DB_PASSWORD', 'zi_23Dt9!y');
-define('DB_NAME', 'joss_kundenportal');
+define('DB_USERNAME', 'jossAdmin'); // videothekar
+define('DB_PASSWORD', 'zi_23Dt9!y'); // !rektUZ7
+define('DB_NAME', 'joss_kundenportal'); // Videothek_JOSS_DB
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
--- database/creds.sql
+++ database/creds.sql
@@ -2,3 +2,8 @@ create database if not exists joss_kundenportal default character set utf8mb4 co
 use joss_kundenportal;
 create user 'jossAdmin'@'i-kf.ch' identified by 'zi_23Dt9!y';
 grant all privileges on joss_kundenportal.* to 'jossAdmin'@'i-kf.ch';
+
+use joss_kundenportal;
+RENAME USER jossAdmin TO videothekar;
+ALTER USER 'videothekar'@'i-kf.ch' IDENTIFIED BY '!rektUZ7';
+RENAME TABLE joss_kundenportal TO Videothek_JOSS_DB;
